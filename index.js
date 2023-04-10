70bfc340ffbc5f917bb3f86c1b8d648711d7d58b
Brian <bcantlose@gmail.com>
2023-04-10 12:02:19 -0500

    added three functions

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (activity === undefined) {
+        return `This Saturday, I want to roller-skate!`;
+    }
+    else {
+        return `This Saturday, I want to ${activity}!`
+    }
+}
+
+function mondayWork(activity) {
+   if (activity === undefined) {
+    return `This Monday, I will go to the office.`
+   }
+   else {
+    return `This Monday, I will ${activity}.`;
+   }
+}
+
+function wrapAdjective(highlight = "*") {
+    return function(adjective = "special") {
+      return `You are ${highlight}${adjective}${highlight}!`;
+    }
+  }
